8cf09c18787bd99d70536e98208f8d03cb45062a
trggr <tashepkov@gmail.com>
2025-09-13 14:08:14 -0400

    Update util_test.clj

--- test/timlib/util_test.clj
+++ test/timlib/util_test.clj
@@ -1,5 +1,5 @@
 (ns timlib.util-test
-  (:require [timlib.util :refer [index-of]]
+  (:require [timlib.util :refer [index-of pad]]
             [clojure.test :refer [deftest is]]))
 
 
@@ -7,3 +7,9 @@
 (deftest test-index-of
   (is (= 3
          (index-of [10, 20, 30, 40, 50] 40))))
+
+(deftest test-pad
+  (is (= [1, 2, 3, nil, nil]
+         (pad [1, 2, 3] 5)))
+  (is (= "123...."
+         (apply str (pad "123" 7 ".")))))
